753dd08ab2008616942063380cd2099d40b03bb9
BT_2S14_24 <email@gmail.com>
2024-10-07 10:39:34 -0400

    Realized the program does not support negative #'s. Added comments. Created the method for -#'s to be included in the program. Fixed up methods in SlopeSolver.java. Finished up the word with x3 by making a returnable string and a method to calculate it's y value.

--- src/Runner.java
+++ src/Runner.java
@@ -9,33 +9,115 @@ public class Runner {
         System.out.print("Please input the second point: ");
         String second = s.nextLine();
 
-        String intValue = first.replaceAll("[^-100-100]", "");
-        System.out.println(intValue);
-        String str = first;
-        str = str.replaceAll("\\(", "").replaceAll("\\)","");
-        str = str. replaceAll(", $", "");
-        System.out.println(str);
+        //Welcomes the user and collects two coordinate points
 
-        System.out.println((str));
-        /*String xx1 = first.substring(1,2);
-        String xx2 = second.substring(1,2);
-        String yy1 = first.substring(3,4);
-        String yy2 = second.substring(3,4);
+        first = first.replace("(", "").replace(")", "").trim();
+        second = second.replace("(", "").replace(")", "").trim();
+        first = first.replace(",", "").trim();
+        second = second.replace(",", "").trim();
 
+        //Removes every '(' ')' and ',' from the input string.
 
-        double x1 = Double.parseDouble(xx1);
-        double x2 = Double.parseDouble(xx2);
-        double y1 = Double.parseDouble(yy1);
-        double y2 = Double.parseDouble(yy2);
+        double x1 = 0.0;
+        double y1 = 0.0;
+        double x2 = 0.0;
+        double y2 = 0.0;
 
+        // Initializes the variables
 
-        SlopeSolver wow = new SlopeSolver(x1,x2,y1,y2);
+        if (first.length() == 4)
+        {
+            String xx1 = first.substring(1,2);
+            String yy1 = first.substring(3,4);
+            x1 = Double.parseDouble(xx1);
+            y1 = Double.parseDouble(yy1);
+            x1 = x1 * -1;
+            y1 = y1 * -1;
+        }
+
+        if (second.length() == 4)
+        {
+            String xx2 = second.substring(1,2);
+            String yy2 = second.substring(3,4);
+            x2 = Double.parseDouble(xx2);
+            y2 = Double.parseDouble(yy2);
+            x2 = x2 * -1;
+            y2 = y2 * -1;
+        }
+
+        //takes out the numbers from the input in case both of the inputs are negative
+
+        if(first.length() == 3)
+        {
+            if(first.indexOf("-") == 0)
+            {
+                String xx1 = first.substring(1,2);
+                String yy1 = first.substring(2,3);
+                x1 = Double.parseDouble(xx1);
+                y1 = Double.parseDouble(yy1);
+                x1 = x1 * - 1;
+            }
+
+            if(first.indexOf("-") == 1)
+            {
+                String xx1 = first.substring(0,1);
+                String yy1 = first.substring(2,3);
+                x1 = Double.parseDouble(xx1);
+                y1 = Double.parseDouble(yy1);
+                y1 = y1 * - 1;
+            }
+        }
+
+        if(second.length() == 3)
+        {
+            if(second.indexOf("-") == 0)
+            {
+                String xx2 = second.substring(1,2);
+                String yy2 = second.substring(2,3);
+                x2 = Double.parseDouble(xx2);
+                y2 = Double.parseDouble(yy2);
+                x2 = x2 * - 1;
+            }
+
+            if(second.indexOf("-") == 1)
+            {
+                String xx2 = second.substring(0,1);
+                String yy2 = second.substring(2,3);
+                x2 = Double.parseDouble(xx2);
+                y2 = Double.parseDouble(yy2);
+                y2 = y2 * - 1;
+            }
+        }
+
+        //takes out the numbers from the input in case only 1 is negative. Uses indexOf() to figure out if the x or the y is negative to later multiply it by -1.
+
+        if(first.length() == 2)
+        {
+            String xx1 = first.substring(0,1);
+            String yy1 = first.substring(1,2);
+            x1 = Double.parseDouble(xx1);
+            y1 = Double.parseDouble(yy1);
+        }
+
+        if (second.length() == 2)
+        {
+            String xx2 = second.substring(0,1);
+            String yy2 = second.substring(1,2);
+            x2 = Double.parseDouble(xx2);
+            y2 = Double.parseDouble(yy2);
+        }
+
+        //returns the values if both of the values are positive
+
+        SlopeSolver wow = new SlopeSolver(x1,x2,y1,y2);  //creates the object via the gathered values
         System.out.println(wow.toString());
 
 
-       /* System.out.print("Input a 3rd X coordinate: ");
-        String xx3 = s.nextLine();
-        double x3 = Double.parseDouble(xx3);
-       */
+       System.out.print("Input a 3rd X coordinate: ");  //third x point
+       String xx3 = s.nextLine();
+       double x3 = Double.parseDouble(xx3);
+        System.out.println(wow.pointFinder(x3));
+
+
     }
 }
--- src/SlopeSolver.java
+++ src/SlopeSolver.java
@@ -9,12 +9,12 @@ public class SlopeSolver {
     private double x2;
     private double y1;
     private double y2;
-    private double x3;
-    private double slope;
+
+    //initiates variables for the class
 
 
 
-    NumberFormat formatter = new DecimalFormat("#0.00");
+    NumberFormat formatter = new DecimalFormat("#0.00");  //decimal rounding format
 
     SlopeSolver(double x1, double x2, double y1, double y2)
     {
@@ -24,6 +24,7 @@ public class SlopeSolver {
         this.y2 = y2;
     }
 
+    //constructor for the runner class
 
     public String slope()
     {
@@ -36,6 +37,7 @@ public class SlopeSolver {
         else
             return (y2-y1) + "/" + (x2-x1);
     }
+    //returns the slope as a String and rounds it if needed
 
     public double yIntercept()
     {
@@ -43,12 +45,31 @@ public class SlopeSolver {
         double yIntercept = y1 - slope*x1;
         return yIntercept;
     }
-
+    //calculates the y-int
     public double distance()
     {
         double distance = sqrt(pow((x2-x1),2) + pow((y2-y1),2));
         return distance;
     }
+    //calculates the distance between the points
+    public double thirdPoint(double x3)
+    {
+        double wow = ((y2 - y1) / (x2 - x1)) * x3 + yIntercept();
+        return wow;
+    }
+    //gets the 3rd point after x3 is given
+    public String printIntercept()
+    {
+        String intercept = "";
+        if(yIntercept() > 0 )
+        {
+            intercept = "+ " + yIntercept();
+        }
+
+        return intercept;
+    }
+
+    //used so I can print out the y-int neatly with the correct sign
 
     public String toString()
     {
@@ -56,15 +77,15 @@ public class SlopeSolver {
         str += "Second pair: " + "(" + (int)Math.round(x2) + "," + (int)Math.round(y2) + ")\n";
         str += "Slope of the line: " + slope() + "\n";
         str += "Y-intercept: (" + (int)Math.round(yIntercept()) + ", 0)" + "\n";
-        str += "Slope-int form: y = " + slope() + "x" + " + " + yIntercept() + "\n";
+        str += "Slope-int form: y = " + slope() + "x "  + printIntercept() + "\n";
         str += "Distance between points: " + formatter.format(distance());
 
         return str;
     }
 
-    public String pointFinder()
+    public String pointFinder(double x3)
     {
-        String point = "hi";
+        String point = "The coordinate point with that value is: (" + x3 + "," + thirdPoint(x3) + ")";
         return point;
     }
 
